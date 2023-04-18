b6a1380796e4525738c58634159591cb72728b4c
Gurmanich <vadya.com40@gmail.com>
2023-04-18 09:55:39 +0700

    PW5

---
+++ PW5/PW5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW5
+{
+    internal class Program
+    {
+
+        static string[] SplitText (string userText)
+        {
+            return userText.Split(' ');
+        }
+        
+        static void Result(string[] userText)
+        {
+            foreach (string word in userText)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение ");
+            string userText = Console.ReadLine();
+            string[] words = SplitText(userText);
+            Result(words);
+            
+
+        }
+    }
+}
---
+++ PW5/PW5Note/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW5Note
+{
+    internal class Program
+    {
+        static void Print(char[,] Map)
+        {
+            for (int i = 0;  i < Map.GetLength(0); i++)
+            {
+                for (int j = 0; j < Map.GetLength(1); j++)
+                {
+                    Console.Write(Map[i, j] == '.' ? ' ' : Map[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void ToColor(char[,] Map, int PosX, int PosY) 
+        {
+            if (Map[PosX, PosY] == '.')
+            {
+                Map[PosX, PosY] = '+';
+                ToColor(Map, PosX - 1, PosY);
+                ToColor(Map, PosX, PosY + 1);
+                ToColor(Map, PosX + 1, PosY);
+                ToColor(Map, PosX, PosY - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            char[,] map = new char[,]
+            {
+               {'.','.', '.', '.'},
+               {'+','.','.', '.',}
+            };
+            Print(map);
+            Console.WriteLine();
+            ToColor(map, 1, 1);
+            Print(map);
+        }
+
+    }
+       
+}
+
